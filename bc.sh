0c959b10d8a0b490bbeab4bb1b20c0eec5793983
Yuri Kanivetsky <yuri.kanivetsky@gmail.com>
2024-07-08 16:43:31 +0300

    Add --same-dockerfile

--- bc.sh
+++ bc.sh
@@ -36,6 +36,15 @@ while [ $# -gt 0 ]; do
             g_args+=("$1" "$2")
             shift 2
             ;;
+        --same-dockerfile)
+            if [ "$cur_svc" = p_app ]; then
+                printf '%s\n' "$0: $1 can not be specified for an app" >&2
+                exit 1
+            fi
+            n_cur_svc[dockerfile]=${p_app[dockerfile]}
+            g_args+=("$1")
+            shift
+            ;;
         --build-arg)
             declare -n n_build_args=${n_cur_svc[build_args]}
             n_build_args+=("$1" "$2")
