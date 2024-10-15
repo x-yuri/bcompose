fff83f4c3285950a224139c67c7929ff2b1a4806
Yuri Kanivetsky <yuri.kanivetsky@gmail.com>
2024-10-15 16:20:05 +0300

    Prepare for the following commit (indents)

--- bc.sh
+++ bc.sh
@@ -621,86 +621,86 @@ case "$1" in
             done
         fi
 
-        if [ "${p_app[http]}" ]; then
-            if ! [ "`cid haproxy haproxy`" ]; then
-                docker build -t bcompose-haproxy \
-                             -f "$g_bc_dir/Dockerfile-haproxy" \
-                             "$g_bc_dir"
-                h start haproxy
-                start_haproxy_container
-            fi
-        fi
-
-        for (( i = 1; i <= "${p_app[replicas]}"; i++ )); do
             if [ "${p_app[http]}" ]; then
-                h "disable ${p_app[name]}-$i"
-                r=`haproxy_cmd "disable server ${p_app[name]}/s$i"`
-                if [ "$r" ]; then
-                    printf '%s\n' "$r"
+                if ! [ "`cid haproxy haproxy`" ]; then
+                    docker build -t bcompose-haproxy \
+                                 -f "$g_bc_dir/Dockerfile-haproxy" \
+                                 "$g_bc_dir"
+                    h start haproxy
+                    start_haproxy_container
                 fi
-
-                if [ -v p_upstream[@] ]; then
-                    cid=`cid "${p_upstream[name]}" "${p_upstream[name]}-$i"`
-                    if [ "$cid" ]; then
-                        h "stop ${p_upstream[name]}-$i"
-                        docker stop -- "$cid"
-                    fi
-                fi
-
-                cid=`cid "${p_app[name]}" "${p_app[name]}-$i"`
-                if [ "$cid" ]; then
-                    h "stop ${p_app[name]}-$i"
-                    docker stop -- "$cid"
-                fi
-
-                if [ -v p_upstream[@] ]; then
-                    h "start ${p_upstream[name]}-$i"
-                    start_svc_container p_upstream "$i"
-                fi
-
-                h "start ${p_app[name]}-$i"
-                start_svc_container p_app "$i"
-
-                h "enable ${p_app[name]}-$i"
-                r=`haproxy_cmd "enable server ${p_app[name]}/s$i"`
-                if [ "$r" ]; then
-                    printf '%s\n' "$r"
-                fi
-
-                h wait for status up
-                while true; do
-                    status=`get_server_status "s$i"`
-                    check_status=${status#*|}
-                    status=${status%|*}
-                    if [ "$status" = UP ] && [[ "$check_status" =~ ^L[467]OK$ ]]; then
-                        break
-                    fi
-                    sleep 1
-                done
-            else
-                if [ -v p_upstream[@] ]; then
-                    cid=`cid "${p_upstream[name]}" "${p_upstream[name]}-$i"`
-                    if [ "$cid" ]; then
-                        h "stop ${p_upstream[name]}-$i"
-                        docker stop -- "$cid"
-                    fi
-                fi
-
-                cid=`cid "${p_app[name]}" "${p_app[name]}-$i"`
-                if [ "$cid" ]; then
-                    h "stop ${p_app[name]}-$i"
-                    docker stop -- "$cid"
-                fi
-
-                if [ -v p_upstream[@] ]; then
-                    h "start ${p_upstream[name]}-$i"
-                    start_svc_container p_upstream "$i"
-                fi
-
-                h "start ${p_app[name]}-$i"
-                start_svc_container p_app "$i"
             fi
-        done
+
+            for (( i = 1; i <= "${p_app[replicas]}"; i++ )); do
+                if [ "${p_app[http]}" ]; then
+                    h "disable ${p_app[name]}-$i"
+                    r=`haproxy_cmd "disable server ${p_app[name]}/s$i"`
+                    if [ "$r" ]; then
+                        printf '%s\n' "$r"
+                    fi
+
+                    if [ -v p_upstream[@] ]; then
+                        cid=`cid "${p_upstream[name]}" "${p_upstream[name]}-$i"`
+                        if [ "$cid" ]; then
+                            h "stop ${p_upstream[name]}-$i"
+                            docker stop -- "$cid"
+                        fi
+                    fi
+
+                    cid=`cid "${p_app[name]}" "${p_app[name]}-$i"`
+                    if [ "$cid" ]; then
+                        h "stop ${p_app[name]}-$i"
+                        docker stop -- "$cid"
+                    fi
+
+                    if [ -v p_upstream[@] ]; then
+                        h "start ${p_upstream[name]}-$i"
+                        start_svc_container p_upstream "$i"
+                    fi
+
+                    h "start ${p_app[name]}-$i"
+                    start_svc_container p_app "$i"
+
+                    h "enable ${p_app[name]}-$i"
+                    r=`haproxy_cmd "enable server ${p_app[name]}/s$i"`
+                    if [ "$r" ]; then
+                        printf '%s\n' "$r"
+                    fi
+
+                    h wait for status up
+                    while true; do
+                        status=`get_server_status "s$i"`
+                        check_status=${status#*|}
+                        status=${status%|*}
+                        if [ "$status" = UP ] && [[ "$check_status" =~ ^L[467]OK$ ]]; then
+                            break
+                        fi
+                        sleep 1
+                    done
+                else
+                    if [ -v p_upstream[@] ]; then
+                        cid=`cid "${p_upstream[name]}" "${p_upstream[name]}-$i"`
+                        if [ "$cid" ]; then
+                            h "stop ${p_upstream[name]}-$i"
+                            docker stop -- "$cid"
+                        fi
+                    fi
+
+                    cid=`cid "${p_app[name]}" "${p_app[name]}-$i"`
+                    if [ "$cid" ]; then
+                        h "stop ${p_app[name]}-$i"
+                        docker stop -- "$cid"
+                    fi
+
+                    if [ -v p_upstream[@] ]; then
+                        h "start ${p_upstream[name]}-$i"
+                        start_svc_container p_upstream "$i"
+                    fi
+
+                    h "start ${p_app[name]}-$i"
+                    start_svc_container p_app "$i"
+                fi
+            done
 
         if ! [ "$detach" ]; then
             "$0" ${g_args[@]+"${g_args[@]}"} \
